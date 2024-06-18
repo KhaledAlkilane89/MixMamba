2568dc98c64a99edc3c6b3ffa49cb4ed0a08fe85
khaled <60028253+khaled-alkilane@users.noreply.github.com>
2024-06-18 13:43:50 +0800

    Add files via upload

---
+++ scripts/long_term_forecast/ETT_script/PatchTST_ETTh1_912.sh
@@ -0,0 +1,27 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=PatchTST
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_912 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 912 \
+  --e_layers 1 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --n_heads 2 \
+  --itr 1 \
+  --train_epochs 2
+
